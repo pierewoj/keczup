439b1aba0c0f6a2d35f985d5f14b99e16c333a13
Jakub Pierewoj <jp360641@brown13.mimuw.edu.pl>
2015-03-20 12:31:35 +0100

    Implementation of UART message processiong

--- Pomidor15/src/lowlevel/communication.c
+++ Pomidor15/src/lowlevel/communication.c
@@ -5,14 +5,53 @@
  *      Author: Kuba
  */
 
-//processes a message received by UART. Messages shuold end with '\n'.
+#include "global.h"
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * processes a message received by UART. Messages should end with '\n'.
+ */
 void messageProcessor(char* msg, int msgLength)
+{
+	char command[20]; //maximum command length is 20
+	sscanf(msg,"%20s",&command);
+	if(!strcmp(command, "START")) //received "START" command
+	{
+		//do something
+	}
+}
+
+/*
+ * sends message via UART
+ */
+void sendMessage(char* msg, int msgLength)
 {
 
 }
 
-//sends a message via UART
-void sendMessage(char* msg, int msgLength)
+/*
+ * sends a message via UART, max msg length is 1000
+ * message has to end with newline character ('\n')
+ */
+void sendMessageNewline(char* msg)
 {
+	int i=0;
+	for(i=0;i<1000;i++)
+	{
+		if( msg[i] == '\n')
+		{
+			sendMessage(msg, i+1);
+			return;
+		}
+	}
+}
 
+//sends values of state variables via UART
+void sendAllVariables(void)
+{
+	char messageBuffer[400];
+	snprintf(messageBuffer, 400, "%d\n", state);
+
+	sendMessageNewline(messageBuffer);
 }
