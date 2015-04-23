ff569fc25e87c0281703428a367332a104e067dc
poskart <piotrpos93@gmail.com>
2015-04-24 00:15:14 +0200

    Revert "resolved conflicts"
    This reverts commit 9786f499774450af5f2d2b44a8e75c44912be793.

--- Pomidor15/src/lowlevel/effectors.c
+++ Pomidor15/src/lowlevel/effectors.c
@@ -86,6 +86,7 @@ void setLeftPWM(double v)
 
 //for sending it to PC
 	pwmLeft = v;
+>>>>>>> 3d8b918 Ultra + engine saving system
 
 	if (v > 0)
 	{
@@ -124,6 +125,30 @@ void setLeftPWM(double v)
 	{
 		changeState(STATE_FAIL, REASON_WHEEL_BLOCKED);
 	}
+}
+
+void setLeftPWM(double v)
+{
+	//make sure v is in [-1;1]
+	v = fmin(1, fmax(-1, v));
+	double modul = fabs(v);
+
+	//for sending it to PC
+	pwmLeft = v;
+
+	if (v > 0)
+	{
+		GPIO_ResetBits(GPIOC, GPIO_Pin_9);
+		GPIO_SetBits(GPIOC, GPIO_Pin_8);
+	}
+	else
+	{
+		GPIO_ResetBits(GPIOC, GPIO_Pin_8);
+		GPIO_SetBits(GPIOC, GPIO_Pin_9);
+	}
+
+	if (modul < 1.0000001)
+		TIM2->CCR4 = modul * 1000;
 }
 
 //Motor stop functions using low/high impedance
