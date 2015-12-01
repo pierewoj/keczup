2709a2e82eb87004a02d4c9d72c353d49a079237
poskart <piotrpos93@gmail.com>
2015-12-01 15:36:07 +0100

    The last commit for 1.6 from Lodz

--- Pomidor15/src/lowlevel/config.c
+++ Pomidor15/src/lowlevel/config.c
@@ -365,7 +365,8 @@ void TIMERs_Config(void)
 	TIM17->ARR = 10;	         // Auto reload value [ms]
 	TIM17->DIER = TIM_DIER_UIE;  // Enable update interrupt (timer level)
 	TIM17->CR1 = TIM_CR1_CEN;    // Enable timer
-
+	
+	//Timer 15 for ultra trigger
 	TIM15->PSC = 39;	         // Set prescaler to 40 (PSC + 1); one tick-1us
 	TIM15->ARR = 10;	         // Auto reload value [us]
 	TIM15->DIER = TIM_DIER_UIE;  // Enable update interrupt (timer level)
